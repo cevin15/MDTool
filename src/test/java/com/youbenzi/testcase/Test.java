38e366c285ca33928bffbee0be07c28e8f701c02
杨英强 <cevin15@qq.com>
2017-06-05 18:01:51 +0800

    Test

---
+++ src/test/java/com/youbenzi/testcase/Test.java
@@ -0,0 +1,23 @@
+package com.youbenzi.testcase;
+
+import java.io.File;
+
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		String fileName = Test.class.getResource("/").getFile();
+		System.out.println(fileName);
+		File file = new File(fileName.substring(0, fileName.length() - "MDTool/build/classes/main/".length()) + "tmp_1");
+		System.out.println(file.exists());
+		file.mkdir();
+		System.out.println(file.exists());
+		System.out.println(file.getPath());
+		System.out.println("end");
+	}
+	
+	public static void main(String[] args) {
+		new Test().test();
+	}
+}
