5106ebcc66da60aedf80de9e50160461d2858ed1
杨英强 <cevin15@qq.com>
2018-03-29 12:07:10 +0800

    格式化代码

--- src/main/java/com/youbenzi/mdtool/markdown/MDToken.java
+++ src/main/java/com/youbenzi/mdtool/markdown/MDToken.java
@@ -4,7 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MDToken {
-	
+
 	public static String QUOTE = ">";
 	public static String CODE = "```";
 	public static String CODE_BLANK = "    ";
@@ -23,14 +23,14 @@ public class MDToken {
 	public static String STRIKE_WORD = "~~";
 	public static String CODE_WORD = "`";
 	public static String ROW = "  ";
-	
-	public static Map<String, String> PLACEHOLDER_MAP = new LinkedHashMap<String, String>() {	//需要显示的特殊符号的占位符
+
+	public static Map<String, String> PLACEHOLDER_MAP = new LinkedHashMap<String, String>() { // 需要显示的特殊符号的占位符
 		private static final long serialVersionUID = 5649442662460683378L;
 		{
 			put("\\\\", "$BACKSLASH");
 			put("\\" + IMG, "$IMG");
 			put("\\" + LINK, "$LINK");
-//			put("\\" + BOLD_WORD, "$BOLDWORD");	//因为有 ITALIC_WORD_2 的存在，所以不需要这个
+			// put("\\" + BOLD_WORD, "$BOLDWORD"); //因为有 ITALIC_WORD_2 的存在，所以不需要这个
 			put("\\" + ITALIC_WORD, "$ITALICWORD");
 			put("\\" + ITALIC_WORD_2, "$2ITALICWORD");
 			put("\\" + STRIKE_WORD, "$STRIKEWORD");
@@ -38,29 +38,29 @@ public class MDToken {
 			put("\\", "");
 		}
 	};
-	
-	public static BlockType convert(String mdToken){
-		if(mdToken.equals(QUOTE)){
+
+	public static BlockType convert(String mdToken) {
+		if (mdToken.equals(QUOTE)) {
 			return BlockType.QUOTE;
-		}else if(mdToken.equals(CODE)){
+		} else if (mdToken.equals(CODE)) {
 			return BlockType.CODE;
-		}else if(mdToken.equals(HEADLINE)){
+		} else if (mdToken.equals(HEADLINE)) {
 			return BlockType.HEADLINE;
-		}else if(mdToken.equals(IMG)){
+		} else if (mdToken.equals(IMG)) {
 			return BlockType.IMG;
-		}else if(mdToken.equals(BOLD_WORD1) || mdToken.equals(BOLD_WORD2)){
+		} else if (mdToken.equals(BOLD_WORD1) || mdToken.equals(BOLD_WORD2)) {
 			return BlockType.BOLD_WORD;
-		}else if(mdToken.equals(ITALIC_WORD) || mdToken.equals(ITALIC_WORD_2)){
+		} else if (mdToken.equals(ITALIC_WORD) || mdToken.equals(ITALIC_WORD_2)) {
 			return BlockType.ITALIC_WORD;
-		}else if(mdToken.equals(STRIKE_WORD)){
+		} else if (mdToken.equals(STRIKE_WORD)) {
 			return BlockType.STRIKE_WORD;
-		}else if(mdToken.equals(CODE_WORD)){
+		} else if (mdToken.equals(CODE_WORD)) {
 			return BlockType.CODE_WORD;
-		}else if(mdToken.equals(LINK)){
+		} else if (mdToken.equals(LINK)) {
 			return BlockType.LINK;
-		}else if(mdToken.equals(ROW)){
+		} else if (mdToken.equals(ROW)) {
 			return BlockType.ROW;
-		}else {
+		} else {
 			return BlockType.NONE;
 		}
 	}
